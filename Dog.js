2e451a10744405a94cfe11f1ed1502dce83b275f
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-03-16 21:12:33 -0400

    import and export modules

--- Dog.js
+++ Dog.js
@@ -1,1 +1,16 @@
-// Create the Dog class here
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    const { name } = this;
+    return `
+        <div>
+            ${name}
+        </div>
+    `;
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Remember to import the data and Dog class!
+import dogData from "./data.js";
+import Dog from "./Dog.js";
+
+function render() {
+  document.getElementById("dog").innerHTML = dog.getDogHtml();
+}
+
+const dog = new Dog(dogData);
+
+render();
