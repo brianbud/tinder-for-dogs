00a22d5794f2085e7872005cef4526c584e355d8
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-03-21 20:39:59 -0400

    render img from class

--- Dog.js
+++ Dog.js
@@ -4,7 +4,20 @@ class Dog {
   }
 
   getCharacterHtml() {
-    return ``;
+    return `
+    <div class="img-container">
+         <img class="dog-img" src="./images/dog-teddy.jpg" alt="" />
+         <div class="dog-info">
+           <p class="dog-title">Teddy, 30</p>
+           <p class="dog-tagline">How you doing?</p>
+         </div>
+       </div>
+       <section id="actions" class="action-btns">
+         <img src="images/icon-cross.png" alt="" />
+         <img src="images/icon-heart.png" alt="" />
+       </section>
+ 
+    `;
   }
 }
 
--- index.js
+++ index.js
@@ -2,20 +2,9 @@ import dogs from "./data.js";
 import Dog from "./Dog.js";
 
 function render() {
-  document.getElementById("dog").innerHTML = `
-   <div class="img-container">
-        <img class="dog-img" src="./images/dog-teddy.jpg" alt="" />
-        <div class="dog-info">
-          <p class="dog-title">Teddy, 30</p>
-          <p class="dog-tagline">How you doing?</p>
-        </div>
-      </div>
-      <section id="actions" class="action-btns">
-        <img src="images/icon-cross.png" alt="" />
-        <img src="images/icon-heart.png" alt="" />
-      </section>
-
-   `;
+  document.getElementById("dog").innerHTML = dog.getCharacterHtml();
 }
+
+const dog = new Dog();
 
 render();
